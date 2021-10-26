ddd5638b64994dcd60fd796b67a9d1f814999288
Milton <tomilton@hotmail.com>
2021-10-26 14:32:51 -0500

    init project

---
+++ src/main/java/com/nequi/adaptadordynamodbstreamsreactivos/AdaptadorDynamodbStreamsReactivosApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.adaptadordynamodbstreamsreactivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptadorDynamodbStreamsReactivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaptadorDynamodbStreamsReactivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nequi/adaptadordynamodbstreamsreactivos/AdaptadorDynamodbStreamsReactivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.adaptadordynamodbstreamsreactivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptadorDynamodbStreamsReactivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
